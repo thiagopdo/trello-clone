5318935b5a23866bf151788039016fa84ff09477
thiagopdo <thiago.falconer@gmail.com>
2024-05-04 18:50:18 -0300

    added card component

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,4 +1,5 @@
-import { ColumnContainer, ColumnTitle, CardContainer } from "./styles";
+import { Card } from "./Card";
+import { ColumnContainer, ColumnTitle } from "./styles";
 
 type ColumnProps = {
   text: string;
@@ -8,9 +9,9 @@ export const Column = ({ text }: ColumnProps) => {
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      <CardContainer>Generate App</CardContainer>
-      <CardContainer>Learn Typescript</CardContainer>
-      <CardContainer>Begin static typing</CardContainer>
+      <Card text="Generate App scaffold" />
+      <Card text="LKearn TypeScript" />
+      <Card text="BEgin static typng learning" />
     </ColumnContainer>
   );
 };
