7a35d7373148ba164e569a7622203231e62031ec
thiagopdo <thiago.falconer@gmail.com>
2024-05-06 17:34:58 -0300

    added Column drag and drop functionality

--- src/Column.tsx
+++ src/Column.tsx
@@ -2,7 +2,11 @@ import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
 import { ColumnContainer, ColumnTitle } from "./styles";
 import { useAppState } from "./state/AppStateContext";
-import { addTask } from "./state/action";
+import { addTask, moveList } from "./state/action";
+import { useRef } from "react";
+import { useItemDrag } from "./utils/useItemDrag";
+import { useDrop } from "react-dnd";
+import { throttle } from "throttle-debounce-ts";
 
 type ColumnProps = {
   text: string;
@@ -10,11 +14,30 @@ type ColumnProps = {
 };
 
 export const Column = ({ text, id }: ColumnProps) => {
-  const { getTasksByListId, dispatch } = useAppState();
+  const { draggedItem, getTasksByListId, dispatch } = useAppState();
   const tasks = getTasksByListId(id);
+  const ref = useRef<HTMLDivElement>(null);
+  const { drag } = useItemDrag({ type: "COLUMN", id, text });
+
+  /**
+   * create a custom hook to handle the drop functionality
+   * @type {*}
+   */
+  const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover: throttle(200, () => {
+      if (!draggedItem) return;
+      if (draggedItem.type === "COLUMN") {
+        if (draggedItem.id === id) return;
+        dispatch(moveList(draggedItem.id, id));
+      }
+    }),
+  });
+
+  drag(drop(ref));
 
   return (
-    <ColumnContainer>
+    <ColumnContainer ref={ref}>
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => (
         <Card text={task.text} key={task.id} id={task.id} />
